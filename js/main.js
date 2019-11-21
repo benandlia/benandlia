087f1dfcf72e0f1b861ef84cb2091886651e45f4
liaadele <lastein@mac.com>
2019-11-21 09:46:13 -0500

    fleshed it out

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+$( document ).ready(function() {
+
+  //accordions
+  if($('.accordion').length){
+    var allPanels = $('.accordion > dd');
+    var allLines = $('.accordion > dt'); 
+    var allButton = $('.accordion > dt > a > span'); 
+    $('.accordion > dt > a').click(function() {
+        $this = $(this);
+        $target =  $this.parent().next();
+        $button = $this.find('span');
+        if($this.parent().hasClass('open')){
+        	$this.parent().removeClass('open');
+        }
+        if(!$target.hasClass('active')){
+           allPanels.removeClass('active').slideUp();
+           allButton.removeClass('rotate');
+           allLines.removeClass('open')
+           $target.addClass('active').slideDown();
+           $button.addClass('rotate');
+           $this.parent().addClass('open').siblings().removeClass('open');
+        }
+        else if($target.hasClass('active')){
+           allPanels.removeClass('active').slideUp();
+           allButton.removeClass('rotate');
+        }
+        
+      return false;
+    });
+  }
+  
+  //smooth scrolling
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+
+    
+});//end doc ready
